ae2aa505629c2ed23b677c795e4824c2c330a032
TFelber <tom.felber@t-online.de>
2020-07-11 16:18:23 +0200

    Update Test

--- src/bot/bot_client.py
+++ src/bot/bot_client.py
@@ -19,6 +19,7 @@ class BotClient(commands.Bot):
     async def on_ready(self):
         self.check_prefixes()
         print("online")
+        print("Test")
 
     async def on_message(self, message):
         if message.guild.id not in self.permit.guilds and message.channel.id not in self.permit.channels and \
