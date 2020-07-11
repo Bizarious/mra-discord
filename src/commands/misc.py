8ba62196d1533f0d0ee30e4082dca5fd7367b1fc
TFelber <tom.felber@t-online.de>
2020-07-11 16:09:47 +0200

    more possibilities to clear messages

--- src/commands/misc.py
+++ src/commands/misc.py
@@ -11,8 +11,13 @@ class Misc(commands.Cog):
         await ctx.send(content)
 
     @commands.command()
-    async def clear(self, ctx, n=5):
-        await ctx.channel.purge(limit=n)
+    async def clear(self, ctx, n=5, mode=None):
+        if mode is None:
+            async for m in ctx.message.channel.history(limit=n):
+                if m.author == self.bot.user or m.content.startswith(self.bot.prefixes[str(ctx.message.guild.id)]):
+                    await m.delete()
+        elif mode == "all":
+            await ctx.channel.purge(limit=n)
 
     @commands.command()
     async def choose(self, ctx):
@@ -21,8 +26,10 @@ class Misc(commands.Cog):
         for c in vc:
             for m in c.members:
                 if m.id == ctx.message.author.id:
-                    i = random.randint(0, len(c.members))
+                    i = random.randint(0, len(c.members)-1)
                     await ctx.send(c.members[i].mention)
+                    return
+        await ctx.send("You are not in a voice-channel.")
 
 
 def setup(bot):
