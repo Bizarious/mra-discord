00cc8df6d271b6bd4d7c143fc605602af3df0982
bizarious <bizardev@web.de>
2020-10-24 14:03:11 +0200

    renamed shutdown task

--- src/tasks/shutdown_tasks.py
+++ src/tasks/shutdown_tasks.py
@@ -3,7 +3,7 @@ from task.task_control import task
 
 
 @task("Shutdown")
-class Reminder(TimeBasedTask):
+class ShutdownTask(TimeBasedTask):
     def __init__(self, *, author_id, date_string):
         TimeBasedTask.__init__(self, author_id=author_id, date_string=date_string)
         self.delete = True
