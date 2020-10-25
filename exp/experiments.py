07904d6dfc25d2f0851af18e84ac259608af7c24
bizarious <bizardev@web.de>
2020-10-25 15:44:43 +0100

    some experiments

--- exp/experiments.py
+++ exp/experiments.py
@@ -1,5 +1,11 @@
-try:
-    raise RuntimeError("Testing")
-except RuntimeError as e:
-    print(isinstance(e, Exception))
-    raise e
+s = "<p,a,d>Hallo Welt"
+t = ""
+x = ""
+if s.startswith("<"):
+    for i in s:
+        t += i
+        if i == ">":
+            break
+print(t)
+s = s[len(t):]
+print(s)
