d709e48945aa40a29a60a06387a511fe9e9e6ea2
bizarious <bizardev@web.de>
2020-11-10 13:05:06 +0100

    added new container for decorators

--- src/core/containers.py
+++ src/core/containers.py
@@ -17,3 +17,11 @@ class TaskContainer:
     def __init__(self, task_class, name: str):
         self.task_class = task_class
         self.name = name
+
+
+class FctContainer:
+
+    def __init__(self, fct, mode: str, *args):
+        self.fct = fct
+        self.fct_add = mode
+        self.args = args
