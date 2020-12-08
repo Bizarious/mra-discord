5df6642cf4a84d830b3871f1833781b80e7ddbfc
bizarious <bizardev@web.de>
2020-12-08 11:16:26 +0100

    added additional time strings

--- src/core/enums.py
+++ src/core/enums.py
@@ -4,3 +4,5 @@ from enum import Enum
 class Dates(Enum):
     DATE_FORMAT = "%d.%m.%y %H:%M:%S"
     DATE_FORMAT_DETAIL = "%d.%m.%y %H:%M:%S:%f"
+    DATE_FORMAT_DATE_ONLY = "%d.%m.%y"
+    TIME_FORMAT = "%H:%M:%S"
