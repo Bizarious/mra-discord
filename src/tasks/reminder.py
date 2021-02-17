188bc350bc53415143fadd3e55046df2738dc2ba
bizarious <bizardev@web.de>
2021-02-17 17:06:18 +0100

    added min interval

--- src/tasks/reminder.py
+++ src/tasks/reminder.py
@@ -4,13 +4,15 @@ from core.task import task
 
 @task("Reminder")
 class Reminder(TimeBasedTask):
-    def __init__(self, *, author_id, channel_id, server_id=None, date_string, number, label, message, message_args):
+    def __init__(self, *, author_id, channel_id, server_id=None, date_string, label, message, message_args):
         TimeBasedTask.__init__(self, author_id=author_id,
                                channel_id=channel_id,
                                server_id=server_id,
                                date_string=date_string,
                                label=label,
-                               number=number)
+                               min_interval=60
+                               )
+
         self.message = message
         self.message_args = message_args
 
