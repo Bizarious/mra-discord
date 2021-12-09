031877d3aea0c94903e16b41f54fd5cc0069396c
bizarious <bizardev@web.de>
2021-12-09 19:29:34 +0100

    first bot tests

--- mra_discord/core/bot.py
+++ mra_discord/core/bot.py
@@ -1,6 +1,23 @@
 from nextcord.ext import commands
+from mra_discord.core.ext import ExtensionHandler
+from mra_discord.core.ext.mixins import ExtensionHandlerCogMixin
+
+
+class BotExtensionHandler(ExtensionHandler, ExtensionHandlerCogMixin):
+
+    def __init__(self, bot, *paths):
+        ExtensionHandler.__init__(self, bot, *paths)
+        ExtensionHandlerCogMixin.__init__(self)
 
 
 class Bot(commands.Bot):
-    pass
+
+    def __init__(self, *, command_prefix: str = ".", paths: list[str]):
+        commands.Bot.__init__(self, command_prefix=command_prefix)
+        self._extension_handler = BotExtensionHandler(self, *paths)
+        self._extension_handler.load_extensions_from_paths()
+        self._extension_handler.load_extension("System")
+
+    async def on_ready(self):
+        print("Ready")
 
