cf850e9913b74ceb3e9b34051bf2a62b409daa44
bizarious <bizardev@web.de>
2021-12-10 14:34:07 +0100

    refined some imports

--- mra_discord/core/bot.py
+++ mra_discord/core/bot.py
@@ -1,6 +1,9 @@
 from nextcord.ext import commands
-from mra_discord.core.ext import ExtensionHandler
-from mra_discord.core.ext.mixins import ExtensionHandlerCogMixin
+from core.ext import ExtensionHandler
+from core.ext.mixins import ExtensionHandlerCogMixin
+
+import nextcord
+import core.data as data
 
 
 class BotExtensionHandler(ExtensionHandler, ExtensionHandlerCogMixin):
@@ -12,12 +15,25 @@ class BotExtensionHandler(ExtensionHandler, ExtensionHandlerCogMixin):
 
 class Bot(commands.Bot):
 
-    def __init__(self, *, command_prefix: str = ".", paths: list[str]):
-        commands.Bot.__init__(self, command_prefix=command_prefix)
-        self._extension_handler = BotExtensionHandler(self, *paths)
+    def __init__(self, *,
+                 command_prefix: str = ".",
+                 extension_paths: list[str],
+                 data_path: str,
+                 intents: nextcord.Intents = nextcord.Intents.default()
+                 ):
+
+        commands.Bot.__init__(self, command_prefix=command_prefix, intents=intents)
+
+        # the extension handler used to manage all extensions
+        self._extension_handler = BotExtensionHandler(self, *extension_paths)
         self._extension_handler.load_extensions_from_paths()
         self._extension_handler.load_extension("System")
 
+        # the data manager
+        self._data_provider = data.DataProvider(data_path)
+
+        # flags
+        self._running = False
+
     async def on_ready(self):
         print("Ready")
-
